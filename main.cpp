83f277063a11c055d32aa2d88507138bad5ff5b5
Vamsy Vrishank <vamsyvrishank@rocketmail.com>
2025-10-13 21:24:22 -0400

    testing the code

--- main.cpp
+++ main.cpp
@@ -13,18 +13,38 @@ using namespace std;
 #include "PricingEngine.hpp"
 
 int main() {
-    MarketData mkt(105.0, 0.05, 0.2, 0.0);
 
-    PayOffCall call(100.0);
-    PayOffPut put(100.0);
+    MarketData data{};
+    data.spot = 100;
+    data.rate = 0.05;
+    data.vol = 0.2;
+    data.dividend = 0.0;
 
-    EuropeanOption callOpt(call, 1.0);
-    EuropeanOption putOpt(put, 1.0);
+    double K = 100;
+    double T = 1.0;
 
-    const BlackScholesAnalyticEngine engine;
 
-    std::cout << "European Call Price: " << engine.calculate(callOpt, mkt) << std::endl;
-    std::cout << "European Put Price : " << engine.calculate(putOpt, mkt) << std::endl;
+    //payoff
+    PayOffCall callPayoff(K);
+    EuropeanOption callOption(callPayoff , T);
+
+    // BS discrete process
+    BlackScholesProcess BSProcess(data.spot , data.rate , data.vol);
+
+    int nPaths = 1000;
+    int nSteps = 1000;
+
+    MonteCarloEngine engine(BSProcess , nPaths , nSteps);
+
+    double mcCallPrice = engine.calculate(callOption , data);
+    cout << "Monte Carlo Call Option price BS Discrete Process : " << mcCallPrice << endl;
+
+
+    // BS Closed form price
+    BlackScholesAnalyticEngine BSAnalyticsEngine;
+    double bsAnalyticsCallPrice = BSAnalyticsEngine.calculate(callOption , data);
+
+    cout << "BS Closed form Call Option Price : " << bsAnalyticsCallPrice << endl;
 
     return 0;
 }
