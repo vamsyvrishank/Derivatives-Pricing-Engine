34f753e9e6f1e3b7fb4747cdc8f3a362c187bde6
Vamsy Vrishank <vamsyvrishank@rocketmail.com>
2025-10-13 21:22:53 -0400

    adding default constructor

--- MarketData.hpp
+++ MarketData.hpp
@@ -11,6 +11,9 @@ struct MarketData
     double dividend;
 
 
+    //create a default constructor
+    MarketData() = default;
+
     MarketData(double S , double r , double sigma , double q = 0.0) :
     spot(S) , rate(r) , vol(sigma) , dividend(q) {}
 };
