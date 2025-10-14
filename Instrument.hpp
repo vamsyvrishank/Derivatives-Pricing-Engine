0c637ea1eb971955e8cf911c5ceb5536c68f4a06
Vamsy Vrishank <vamsyvrishank@rocketmail.com>
2025-10-13 21:24:12 -0400

    updating instrument class for path dependent options

--- Instrument.hpp
+++ Instrument.hpp
@@ -3,13 +3,15 @@
 
 #include "payoff.hpp"
 
-
+using namespace  std;
 //abstract base class
 
 class Instrument {
     public:
+
         virtual ~Instrument() = default;
         virtual double payoff(double spot) const = 0;
+        virtual double payoff(const vector<double>& path) const = 0;
         virtual double getMaturity() const = 0;
         virtual double getStrike() const = 0;
         virtual bool isCall() const = 0;
@@ -18,18 +20,27 @@ class Instrument {
 
 // concrete class
 class EuropeanOption : public Instrument {
+
     PayOff* payoff_ptr;
     double T;
+
     public:
-    EuropeanOption(const PayOff & p, double expiry) : payoff_ptr(p.clone()), T(expiry) {}
-    ~EuropeanOption() { delete payoff_ptr; }
-    double payoff(double spot) const  override {
-      return (*payoff_ptr)(spot);
-    }
+        EuropeanOption(const PayOff & p, double expiry) : payoff_ptr(p.clone()), T(expiry) {}
+
+        ~EuropeanOption() override { delete payoff_ptr; }
+
+        double payoff(const double spot) const override
+        {
+          return (*payoff_ptr)(spot);
+        }
+    double payoff(const vector<double>& path) const override {
+            return (*payoff_ptr)(path.back());
+        }
 
     double getMaturity() const override {return T;}
 
-    // helper functions to identify call/put type polymorphically
+    // helper functions to identify call/put type
+
     bool isCall() const override { return dynamic_cast<const PayOffCall*>(payoff_ptr) != nullptr; }
     double getStrike() const override {
         if (auto call = dynamic_cast<const PayOffCall*>(payoff_ptr)) return call->getStrike();
@@ -38,4 +49,6 @@ class EuropeanOption : public Instrument {
     }
 
 };
+
+
 #endif
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Derivatives-Pricing-Engine
 A modular high performance c++ derivatives pricing library that implements analytic, tree and monte carlo pricing for various payoffs. with object oriented design patterns ( Strategy , Factory , Decorator , Bridge )
+
+
+Project Structure : 
+
+
+PricingEngine
+├── BlackScholesAnalyticEngine
+├── MonteCarloEngine
+├── BinomialEngine
+└── FiniteDifferenceEngine
+
+StochasticProcess
+├── BlackScholesProcess
+├── HestonProcess
+└── SABRProcess
+
+Instrument
+├── EuropeanOption
+├── AsianOption
+└── BarrierOption
+
+
