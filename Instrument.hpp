81cc467119c24d040c167f629715189214819c01
Vamsy Vrishank <vamsyvrishank@rocketmail.com>
2025-10-28 16:58:56 -0400

    adding american option instrument

--- Instrument.hpp
+++ Instrument.hpp
@@ -15,6 +15,7 @@ class Instrument {
         virtual double getMaturity() const = 0;
         virtual double getStrike() const = 0;
         virtual bool isCall() const = 0;
+        virtual bool isAmerican() const { return false; }
 
 };
 
@@ -25,7 +26,7 @@ class EuropeanOption : public Instrument {
     double T;
 
     public:
-        EuropeanOption(const PayOff & p, double expiry) : payoff_ptr(p.clone()), T(expiry) {}
+        EuropeanOption(const PayOff & p, const double expiry) : payoff_ptr(p.clone()), T(expiry) {}
 
         ~EuropeanOption() override { delete payoff_ptr; }
 
@@ -42,13 +43,43 @@ class EuropeanOption : public Instrument {
     // helper functions to identify call/put type
 
     bool isCall() const override { return dynamic_cast<const PayOffCall*>(payoff_ptr) != nullptr; }
+    bool isAmerican() const override { return false; }
     double getStrike() const override {
-        if (auto call = dynamic_cast<const PayOffCall*>(payoff_ptr)) return call->getStrike();
-        if (auto put = dynamic_cast<const PayOffPut*>(payoff_ptr)) return put->getStrike();
+        if (const auto call = dynamic_cast<const PayOffCall*>(payoff_ptr)) return call->getStrike();
+        if (const auto put = dynamic_cast<const PayOffPut*>(payoff_ptr)) return put->getStrike();
         return 0.0;
     }
-
 };
+
+class AmericanOption : public Instrument {
+    PayOff* payoff_ptr;
+    double T;
+
+public:
+    AmericanOption(const PayOff & p, const double expiry) : payoff_ptr(p.clone()), T(expiry) {}
+    bool isAmerican() const override { return true; }
+    ~AmericanOption() override { delete payoff_ptr; }
+
+    double payoff(const double spot) const override
+    {
+        return (*payoff_ptr)(spot);
+    }
+
+    double payoff(const std::vector<double>& path) const override {
+        return (*payoff_ptr)(path.back());
+    }
+
+    double getMaturity() const override {return T;}
+    bool isCall() const override { return dynamic_cast<const PayOffCall*>(payoff_ptr) != nullptr; }
+
+    double getStrike() const override {
+        if (const auto call = dynamic_cast<const PayOffCall*>(payoff_ptr)) return call->getStrike();
+        if (const auto put = dynamic_cast<const PayOffPut*>(payoff_ptr)) return put->getStrike();
+        return 0.0;
+    }
+};
+
+
 
 
 #endif
